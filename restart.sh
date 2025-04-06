4955259ba2a4f2ecdf9a38482b3f277cdf204c24
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2025-04-06 17:31:27 -0600

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "開始執行 news_json.py..."
-python3 news_json.py
-echo "news_json.py 執行完成。"
+echo "開始執行 news_json.py..." >> ~/deployment/restart.log
+python3 news_json.py >> ~/deployment/restart.log 2>&1
+echo "news_json.py 執行完成。" >> ~/deployment/restart.log

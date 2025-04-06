3d4f0291ab2683c4b3c5e2f8cb465984ec138eec
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2025-04-06 17:36:23 -0600

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-echo "Installing dependencies..."
-pip3 install -r requirements.txt
+echo "Creating virtual environment..."
+python3 -m venv venv
+echo "Activating virtual environment..."
+source venv/bin/activate
+
+echo "Installing dependencies in virtual environment..."
+pip install -r requirements.txt
 echo "Dependencies installed."
 
-echo "Starting execution of news_json.py..."
-python3 news_json.py
+echo "Starting execution of news_json.py using virtual environment..."
+python news_json.py
 echo "news_json.py execution completed."
